de5e1541d39633684f1fd185bf94447c6497c5f3
Brocel <geenens.prof@gmail.com>
2019-09-19 21:45:23 +0200

    edit classe personnage2 : getters et fulldescription()3

--- src/app/shared/models/personnage.ts
+++ src/app/shared/models/personnage.ts
@@ -25,50 +25,51 @@ export class Personnage {
     this.statistics = new Statistics(taille, mass, age);
     //mettre un return descrition et statistics ?
   }
-  //getters
-  getName() {
-    return this.description.name;
-  }
-  getGender() {
-    return this.description.gender;
-  }
-  getRace() {
-    return this.description.race;
-  }
-  getMood() {
-    return this.description.mood;
-  }
-  getJob() {
-    return this.description.job;
-  }
-  getAvatar() {
-    return this.description.avatar;
-  }
-  getStory() {
-    return this.description.story;
-  }
-  getTitle() {
-    return this.description.title;
-  }
-  getLevel() {
-    return this.description.level;
-  }
   // Méthodes
   fullDescription() {
-    return console.log("Bonjour ! Je m'appelle " + this.getName + ", " + this.getRace + " " + this.getJob + " " + this.getGender + " " + this.getTitle + " de niveau " + this.getLevel + ". En général, je suis d'humeur " + this.getMood + ". Les amis, voici mon histoire : " + this.getStory );
+    return console.log("Bonjour ! Je m'appelle " + this.description.name + ", " + this.description.race + " " + this.description.job + " " + this.description.gender + " " + this.description.title + " de niveau " + this.description.level + ". En général, je suis d'humeur " + this.description.mood + ". Les amis, voici mon histoire : " + this.description.story );
   }
 }
 
 class Description {
-  name: string;
-  gender: string;
-  race: string;
-  mood: string; //role play
-  job: string; //classe metier à coder
-  title: string; //titre "honorifique"
-  avatar: string; //url de l'image
-  level: number; // réfléchir au fonctionnement de l'xp
-  story: string; // histoire, description (RP)
+  _name: string;
+  _gender: string;
+  _race: string;
+  _mood: string; //role play
+  _job: string; //classe metier à coder
+  _title: string; //titre "honorifique"
+  _avatar: string; //url de l'image
+  _level: number; // réfléchir au fonctionnement de l'xp
+  _story: string; // histoire, description (RP)
+
+  //getters
+  get name() {
+    return this._name;
+  }
+  get gender() {
+    return this._gender;
+  }
+  get race() {
+    return this._race;
+  }
+  get mood() {
+    return this._mood;
+  }
+  get job() {
+    return this._job;
+  }
+  get avatar() {
+    return this._avatar;
+  }
+  get story() {
+    return this._story;
+  }
+  get title() {
+    return this._title;
+  }
+  get level() {
+    return this._level;
+  }
 
   constructor(
     name: string,
@@ -80,25 +81,25 @@ class Description {
     story: string
   )
   {
-    this.name = name;
-    this.gender = gender;
-    this.race = race;
-    this.mood = mood;
-    this.job = job;
-    this.avatar = avatar;
-    this.story = story;
-    this.title = 'Débutant';
-    this.level = 0;
+    this._name = name;
+    this._gender = gender;
+    this._race = race;
+    this._mood = mood;
+    this._job = job;
+    this._avatar = avatar;
+    this._story = story;
+    this._title = 'Débutant';
+    this._level = 0;
   }
 }
 
 class Statistics {
-  taille: number; // en m
-  mass: number; // en kg
-  age: number; // en années
-  for: number; // force physique
-  def: number; // résistance physique et endurance
-  int: number; // force mentale et charisme
+  _taille: number; // en m
+  _mass: number; // en kg
+  _age: number; // en années
+  _for: number; // force physique
+  _def: number; // résistance physique et endurance
+  _int: number; // force mentale et charisme
 
   constructor(
     taille: number,
@@ -106,11 +107,11 @@ class Statistics {
     age: number
   )
   {
-    this.taille = taille;
-    this.mass = mass;
-    this.age = age;
-    this.for = 10;
-    this.def = 10;
-    this.int = 10;
+    this._taille = taille;
+    this._mass = mass;
+    this._age = age;
+    this._for = 10;
+    this._def = 10;
+    this._int = 10;
   }
 }
