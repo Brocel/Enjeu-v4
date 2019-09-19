e7c79a02417e14f6c5f216b3c3baae72a978efdf
Brocel <geenens.prof@gmail.com>
2019-09-19 14:52:20 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'enjeu-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Enjeu-v4';
+}
