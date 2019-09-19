d1879a328268b3c3a00dc4c5ac23376167cbf32d
Brocel <geenens.prof@gmail.com>
2019-09-19 15:50:21 +0200

    Ajout de home et auth dans public

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PublicRoutingModule } from './public-routing.module';
+import { HomeModule } from './home/home.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    PublicRoutingModule
+    PublicRoutingModule,
+    HomeModule,
+    AuthModule
   ]
 })
 export class PublicModule { }
