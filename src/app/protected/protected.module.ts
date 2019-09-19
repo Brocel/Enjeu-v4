19baf4f2c9ebf56c2fa0d5a99c618b5e869102fd
Brocel <geenens.prof@gmail.com>
2019-09-19 15:54:56 +0200

    Ajout de table jeu dans protected (accueil de la partie membre de l'appli)

--- src/app/protected/protected.module.ts
+++ src/app/protected/protected.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProtectedRoutingModule } from './protected-routing.module';
+import { TableJeuModule } from './table-jeu/table-jeu.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    ProtectedRoutingModule
+    ProtectedRoutingModule,
+    TableJeuModule
   ]
 })
 export class ProtectedModule { }
---
+++ src/app/protected/table-jeu/table-jeu.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TableJeuComponent } from './table-jeu/table-jeu.component';
+
+
+
+@NgModule({
+  declarations: [TableJeuComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TableJeuModule { }
