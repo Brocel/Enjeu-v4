d4382e566123d3bf612f59d4a62d13e9e8cb60f0
Brocel <geenens.prof@gmail.com>
2019-09-19 16:02:05 +0200

    import/export de common module, formsmodule et reactiveformsmodule dans shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule, } from  '@angular/forms';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    CommonModule, //préparation de la factorisation du CommonModule
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
