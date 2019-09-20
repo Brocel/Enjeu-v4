1096c62c7017cb3c1cf41e812b99c850ec25a016
Brocel <geenens.prof@gmail.com>
2019-09-20 17:19:53 +0200

    ajout de signin et signup dans home

--- src/app/public/home/home.module.ts
+++ src/app/public/home/home.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HomeComponent } from './home/home.component';
 import { HomeBannerComponent } from './home-banner/home-banner.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, HomeBannerComponent],
+  declarations: [HomeComponent, HomeBannerComponent, SigninComponent, SignupComponent],
   imports: [
     SharedModule
   ]
