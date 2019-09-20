2e15058a4ffed3cc2e316c3cbd2998d4ed26d768
Brocel <geenens.prof@gmail.com>
2019-09-20 17:45:50 +0200

    routing table jeu

--- src/app/protected/protected-routing.module.ts
+++ src/app/protected/protected-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TableJeuComponent } from './table-jeu/table-jeu.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'table-jeu', component: TableJeuComponent }, // on fait correspondre la route tablejeu au composant table-jeu
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
