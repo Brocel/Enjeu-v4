4fd6c6d1fe63f7f9e898b9484ba115402cc2488e
Brocel <geenens.prof@gmail.com>
2019-09-20 16:08:25 +0200

    redirection /home 2

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { HomeComponent } from './home/home/home.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent }//pointer la route /home sur le component home
+  { path: 'home', component: HomeComponent }, // on fait correspondre la route home au composant home
 ];
 
 @NgModule({
