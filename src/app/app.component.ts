74c7306295c7889d8df53fd3a6d06f58f9c7bd9f
Brocel <geenens.prof@gmail.com>
2019-09-21 13:38:39 +0200

    styles et firebase dans app.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'enjeu-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  constructor() {
+    const firebaseConfig = {
+    apiKey: "AIzaSyCQQ8z6sNICXhX_Pod1L0-YCYZVAeZstps",
+    authDomain: "enjeu-8215e.firebaseapp.com",
+    databaseURL: "https://enjeu-8215e.firebaseio.com",
+    projectId: "enjeu-8215e",
+    storageBucket: "",
+    messagingSenderId: "933409621573",
+    appId: "1:933409621573:web:7bbe1c281413291258852c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  }
 }
--- src/app/public/home/home/home.component.ts
+++ src/app/public/home/home/home.component.ts
@@ -8,23 +8,8 @@ import * as firebase from 'firebase';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() {
-    const firebaseConfig = {
-    apiKey: "AIzaSyCQQ8z6sNICXhX_Pod1L0-YCYZVAeZstps",
-    authDomain: "enjeu-8215e.firebaseapp.com",
-    databaseURL: "https://enjeu-8215e.firebaseio.com",
-    projectId: "enjeu-8215e",
-    storageBucket: "",
-    messagingSenderId: "933409621573",
-    appId: "1:933409621573:web:7bbe1c281413291258852c"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
-  }
+  constructor() { }
 
   ngOnInit() {
   }
-
 }
