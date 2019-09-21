4e83ae99b76289cd8516d2b54b7e191a302882dc
Brocel <geenens.prof@gmail.com>
2019-09-21 14:12:41 +0200

    codage de authguard2

--- src/app/core/services/auth-guard.service.ts
+++ src/app/core/services/auth-guard.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
-import {  }
+import { Observable } from 'rxjs/Observable';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +9,20 @@ export class AuthGuardService implements CanActivate {
 
   constructor(private router: Router) { }
 
-  canActivate(): Observable<boolean>
+  canActivate(): Observable<boolean> | Promise<boolean>| boolean {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.auth().onAuthStateChanged(
+          (user) => {
+            if(user) {
+              resolve(true);
+            } else {
+              this.router.navigate(['/home']);
+              resolve(false);
+            }
+          }
+        );
+      }
+    );
+  }
 }
