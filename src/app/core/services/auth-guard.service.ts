2c3d8a0f6e18e716eaa608a0fa028d3a1c6cfa9b
Brocel <geenens.prof@gmail.com>
2019-09-21 13:47:04 +0200

    codage de authguard1

--- src/app/core/services/auth-guard.service.ts
+++ src/app/core/services/auth-guard.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+import {  }
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  canActivate(): Observable<boolean>
 }
