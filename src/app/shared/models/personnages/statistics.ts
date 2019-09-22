5a616a816a1a6406ff008a18fba6f7f02d325875
Brocel <geenens.prof@gmail.com>
2019-09-22 22:58:38 +0200

    edit des modeles statistics

--- src/app/shared/models/personnages/statistics.ts
+++ src/app/shared/models/personnages/statistics.ts
@@ -1,10 +1,20 @@
-export class Statistics {
-  taille: number;
-  mass: number;
-  age: number;
-  for: number;
-  def: number;
-  int: number;
+import { PersonnageModule } from '../../modules/personnage.module';
+
+export class Statistics extends Race {
+  taille: number; // taille de l'individu en metres
+  mass: number; // masse en kg
+  age: number; // age en années
+  for: number; //force physique
+  def: number; // résistance physique
+  int: number; // intelligence
+  // récupérés de race.ts
+  minTaille: number = race.theMinTaille;
+  maxTaille: number = race.theMaxTaille;
+  minAge: number = race.theMinAge;
+  maxAge: number = race.theMaxAge;
+  mulForRace: number = race.theMulFor;
+  mulDefRace: number = race.theMulDef;
+  mulMass: number = race.theMulMass;
 
   //getters
   get theTaille() {
@@ -28,8 +38,11 @@ export class Statistics {
 
   constructor() {
     //todo taille en fction de Race
+    this.taille = Math.random() * (this.maxTaille - this.minTaille) + this.minTaille;
     //todo mass en fction de Taille
+    this.mass = this.taille  * this.mulMass * 100 / 3;
     //todo age en fonction de Race
+    this.age = Math.random() * (this.maxAge - this.minAge) + this.minAge;
     this.for = 10;
     this.def = 10;
     this.int = 10;
